242e066e3955bf3b6354f8b151306b07309532af
Eric LIAO <d1044171009@gmail.com>
2022-08-04 23:02:22 +0800

    feat: 設定樣版引擎， 新增index 頁面

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
-
-
-
-
-
+const exphbs = require('express-handlebars')
 
 
 const app = express()
 const port = 3000
 
+//setting template engine
+app.engine('hbs', exphbs.engine({ defaultLayot: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs') //啟動引擎
+
+
+
 app.get('/', (req, res) => {
-  res.send('this is homepage')
+  res.render('index')
 })
 
 app.listen(port, () => {
