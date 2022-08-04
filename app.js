88ec1a20c478be590fd4b5964c7a42830d2b20f1
Eric LIAO <d1044171009@gmail.com>
2022-08-04 23:58:55 +0800

    feat: 登入頁面設置完成

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const routes = require('./routes') // 引用路由器
 require('./config/mongoose')  //引用mongoose連線設定
@@ -11,6 +12,8 @@ const port = 3000
 app.engine('hbs', exphbs.engine({ defaultLayot: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs') //啟動引擎
 
+//用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes) //將請求導入路由器
 
 
